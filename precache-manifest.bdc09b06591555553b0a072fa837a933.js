ab557fd9be8f26ce1307f5ef365e8a04f2363808
yesung1 <soye09092@google.com>
2020-06-28 09:48:21 +0900

    Updates

---
+++ precache-manifest.bdc09b06591555553b0a072fa837a933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdd0e43b768d3d5368d3472450f58673",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a25a3d9358d4cf14127f",
+    "url": "/movie_app_2020/static/css/main.3bc3c130.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25a3d9358d4cf14127f",
+    "url": "/movie_app_2020/static/js/main.97547c26.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
